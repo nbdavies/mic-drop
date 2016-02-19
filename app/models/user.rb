9e64c17e275c06d3e09a51cbf87c864e5abd10df
Nick Davies <email": null>
2016-02-19 10:51:34 -0600

    Create models and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   has_many :friends, through: :friendships
   has_many :rsvps
   has_many :subscriptions
-  has_one :venue, foreign_key: "manager_id"
+  has_many :venues, foreign_key: "manager_id"
 end
