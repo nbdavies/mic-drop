56df955c621863a0ad3f3770ef6f6c832a4a553c
Bob Dorff & Sabrina Unrein <pair+bobdorff.sunrein@devbootcamp.com>
2016-02-19 17:08:21 -0600

    More click handling

--- app/assets/javascripts/components/nav_bar.js.jsx
+++ app/assets/javascripts/components/nav_bar.js.jsx
@@ -5,6 +5,7 @@ var LoginButton = React.createClass({
   handleClick: function(e) {
     e.preventDefault();
     this.setState({clicked: true});
+    this.props.onLoginClick({});
   },
   render: function() {
     var button = (this.state.clicked ? <div></div> : <button className="btn right red accent-2" onClick={this.handleClick}>login</button>);
@@ -19,14 +20,18 @@ var NavBar = React.createClass({
     return {logged_in: false};
   },
   handleLoginButtonClick: function() {
-
+    this.setState({login_clicked: true});
   },
   render: function(){
     return(
     <nav className="navigation">
       <a href="#" className="brand-logo">mic_drop</a>
-      <LoginButton />
+      <LoginButton onLoginClick={this.handleLoginButtonClick} />
     </nav>
     );
   }
+});
+
+var Container = React.createClass({
+
 });
