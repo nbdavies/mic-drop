d97c49b0cc5a0b4b12a89db36c612741d6d23682
Bob Dorff & Sabrina Unrein <pair+bobdorff.sunrein@devbootcamp.com>
2016-02-19 15:30:00 -0600

    create login form

---
+++ app/assets/javascripts/components/login_form.js.jsx
@@ -0,0 +1,68 @@
+var LoginBox = React.createClass({
+  handleLoginSubmit: function(form) {
+    $.ajax({
+      url: "/sessions",
+      dataType: 'json',
+      type: 'POST',
+      data: form,
+      success: function(data) {
+        this.setState({data: data})
+        ;
+      }.bind(this),
+      error:function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    return (
+      <div className="LoginBox">
+        <LoginForm />
+      </div>
+    )
+  }
+});
+
+
+var LoginForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  handleUsernameChange: function(e) {
+    this.setState({username: e.target.value});
+  },
+  handlePasswordChange: function(e) {
+    this.setState({password: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var username = this.state.username.trim();
+    var password = this.state.password.trim();
+    if (!username || !email || !password) {
+      return;
+    }
+    this.props.onLoginSubmit({username: username, password: password});
+    this.setState({username: '', password: ''});
+  },
+  render: function() {
+    return (
+      <form className="loginForm">
+        <input
+          type="text"
+          placeholder="username"
+          value={this.state.username}
+          onChange={this.handleUsernameChange} />
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange} />
+        <input type="submit" value="Login" />
+      </form>
+    );
+  }
+});
+
+ReactDOM.render(
+  <LoginBox />,
+  document.getElementById('container')
+);
