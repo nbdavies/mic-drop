3dd3d04254dadfd5f46449869c24df266eb4adac
Bob Dorff & Sabrina Unrein <pair+bobdorff.sunrein@devbootcamp.com>
2016-02-19 16:48:40 -0600

    Create login button, disappears when clicked

--- app/assets/javascripts/components/nav_bar.js.jsx
+++ app/assets/javascripts/components/nav_bar.js.jsx
@@ -1,9 +1,31 @@
-var NavBar = React.createClass({
+var LoginButton = React.createClass({
+  getInitialState: function() {
+    return {clicked: false};
+  },
+  handleClick: function(e) {
+    e.preventDefault();
+    this.setState({clicked: true});
+  },
   render: function() {
+    var button = (this.state.clicked ? <div></div> : <button className="btn right red accent-2" onClick={this.handleClick}>login</button>);
+    return(
+      button
+    );
+  }
+});
+
+var NavBar = React.createClass({
+  getInitialState: function() {
+    return {logged_in: false};
+  },
+  handleLoginButtonClick: function() {
+
+  },
+  render: function(){
     return(
     <nav className="navigation">
-      <a href="#" class="brand-logo">mic_drop</a>
-      <button className="btn right red accent-2">login</button>
+      <a href="#" className="brand-logo">mic_drop</a>
+      <LoginButton />
     </nav>
     );
   }
