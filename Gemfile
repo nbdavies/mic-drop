e07462294e067580f3763865ccc56b94d6d44b42
bobdorff <robertdorff@gmail.com>
2016-02-19 14:43:40 -0600

    half-complete seedfile waiting on login

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'dotenv-rails', :groups => [:development, :test]
 # heroku-friendly Rails app configuration
 
-
+gem 'faker'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-50.times {User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, Faker::Internet.email)}
-Venue.create(lat:)
+50.times {User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, email:Faker::Internet.email)}
+
+10.times {Type.create(name: Faker::Book.genre)}
+
+10.times {Tag.create(name:Faker::Team.creature)}
+
+# 20.times {venue.tags.create()
+
+Event.create(name: "Jazz Stuff", description: 'So much goddamn jazz!', venue_id: 1, )
+
+Event.create(name: "GLWSTX", description: "The heaviest drops!", venue_id: 2, )
+
+Venue.create(lat:41.889816, long:-87.635504, name: "Rock n' Roll Town", type: 1 + rand(9), manager_id:1+ rand(49))
+
+Venue.create(lat:41.891398, long:-87.637435, name: "E-D-AMN", type: 1+ rand(10), manager_id: 1+ rand(50))
+
+# 20.times {Rsvp.create(event.id)}
