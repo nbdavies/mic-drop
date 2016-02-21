3d227970e56ee394b2cf8ee79f09516907c55ee9
Phil Thomas <pthomas551@gmail.com>
2016-02-21 01:03:03 -0600

    Add register and login acceptance tests

--- spec/factories/venue.rb
+++ spec/factories/venue.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     lat  1
     lng  2
     name 'test'
-    manager_id 1
+    association :manager, factory: :user
   end
 end
--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -1,9 +1,19 @@
 describe 'registration', :type => :feature, :js => true do
-  let(:user) { FactoryGirl.build(:user) }
-  let(:venue) { FactoryGirl.build(:venue) }
+
   it 'reveals the registration dialog when register is clicked' do
     visit '/'
     click_button 'register'
-    expect(page).to have_content
+    expect(page).to have_field 'username'
   end
+
+  it 'allows me to register an account' do
+    visit '/'
+    click_button 'register'
+    fill_in 'username', with: 'test'
+    fill_in 'email', with: 'test'
+    fill_in 'password', with: 'password'
+    click_button 'Register'
+    expect(page).to have_button 'Logout'
+  end
+
 end
