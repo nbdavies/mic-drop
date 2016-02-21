c965f4ce28bd889d5a4f1eb81e55eb36cc07b01a
Phil Thomas <pthomas551@gmail.com>
2016-02-21 12:52:45 -0600

    Write tests for add event

--- spec/features/add_event_spec.rb
+++ spec/features/add_event_spec.rb
@@ -3,11 +3,29 @@ describe 'adding an event', :type => :feature, :js => true do
   let(:user) { venue.manager }
   let(:user_id) { venue.manager.id }
 
+  def login
+    visit '/'
+    click_button 'login'
+    fill_in 'username', with: 'test'
+    fill_in 'password', with: 'password'
+    click_button 'Login'
+  end
 
-#   it 'shows an add event button when user has proper permissions' do
-#     current_user.stub(:)
-#     visit '/'
-#     click_button 'register'
-#     expect(page).to have_content
-#   end
+  it 'shows an add event button when user has proper permissions' do
+    login
+    expect(page).to have_button('Add Event')
+  end
+
+  it 'opens a form when add event button is clicked' do
+    login
+    click_button 'Add Event'
+    expect(page).to have_field('name')
+  end
+
+  pending 'allows the user to submit event data' do
+  end
+
+  pending 'shows an event on map once event is submitted' do
+  end
+
 end
