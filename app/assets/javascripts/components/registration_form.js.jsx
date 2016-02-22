d2655345b48197c71af6096ec7d81bc28017be46
Phil Thomas <pthomas551@gmail.com>
2016-02-22 14:24:59 -0600

    Add close button to reg form

--- app/assets/javascripts/components/registration_form.js.jsx
+++ app/assets/javascripts/components/registration_form.js.jsx
@@ -7,6 +7,12 @@ var RegistrationForm = React.createClass({
     this.props.onUserLogin(user)
   },
 
+  handleCancelButtonClick: function(e) {
+    e.preventDefault();
+    $("#modal1").closeModal();
+    $(".lean-overlay").hide();
+  },
+
   handleUsernameChange: function(e) {
     this.setState({username: e.target.value});
   },
@@ -58,14 +64,9 @@ var RegistrationForm = React.createClass({
           value={this.state.password}
           onChange={this.handlePasswordChange} />
         <input type="submit" value="Register" className="btn" onClick={this.handleSubmit} />
+        <Button onClick={this.handleCancelButtonClick}>Cancel</Button>
       </form></div>
 
     );
   }
 });
-
-
-
-
-
-
