e568925b670540261d21e481a1a5e67f9b094e85
Nick Davies & Sabrina Unrein <pair+nbdavies.sunrein@devbootcamp.com>
2016-02-22 09:44:33 -0600

    Add changes from yesterday

--- app/assets/javascripts/components/new_event_form.js.jsx
+++ app/assets/javascripts/components/new_event_form.js.jsx
@@ -1,6 +1,6 @@
 var EventForm = React.createClass({
   getInitialState: function() {
-    return {name: '', date: '', time: '', venue_id: ''};
+    return {name: '', date: '', start_time: '', end_time: '', venue_id: ''};
   },
   handleSubmit: function(e) {
     e.preventDefault();
@@ -26,9 +26,12 @@ var EventForm = React.createClass({
     this.setState({date: e.target.value});
   },
 
-  handleTimeChange: function(e) {
-    this.setState({time: e.target.value});
+  handleStartTimeChange: function(e) {
+    this.setState({start_time: e.target.value});
   },
+
+  handleEndTimeChange: function(e) {
+    this.setState({end_time: e.target.value});
 
   handleDescriptionChange: function(e) {
     this.setState({description: e.target.value});
@@ -72,9 +75,14 @@ var EventForm = React.createClass({
           onChange={this.handleDateChange} />
         <input
           type="time"
-          placeholder="time"
-          value={this.state.time}
-          onChange={this.handleTimeChange} />
+          placeholder="start time"
+          value={this.state.start_time}
+          onChange={this.handleStartTimeChange} />
+        <input
+          type="time"
+          placeholder="end time"
+          value={this.state.end_time}
+          onChange={this.handleEndTimeChange} />
         <textarea
           type="text"
           placeholder="description"
