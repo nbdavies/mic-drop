1e15bce6b1a9b0635b6037119c375263dcc81d7e
Bob Dorff <email": null>
2016-02-22 11:14:06 -0600

    basic pin/infowin classes

--- app/assets/javascripts/components/pin.js.jsx
+++ app/assets/javascripts/components/pin.js.jsx
@@ -1,1 +1,18 @@
-pin.js.jsx
+var Pin = React.createClass({
+  getInitialState: function() {
+    return { clicked: false }
+  },
+
+  var position = {lat: parseFloat(pin.location.lat), lng: parseFloat(pin.location.lng)};
+    var marker = new google.maps.Marker({
+      position: position,
+      title: pin.name
+    });
+
+  render: function(){
+    return (<div id='infoWindow'>
+        InfoWindow event = {this.state.event}
+      </div>
+      );
+  }
+})
