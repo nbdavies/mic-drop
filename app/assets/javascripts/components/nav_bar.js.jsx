bc68ff4f8966127d93bf78d71d0ebb6823089baf
Bob Dorff & Sabrina Unrein <pair+bobdorff.sunrein@devbootcamp.com>
2016-02-23 18:30:21 -0600

    Add profile picture

--- app/assets/javascripts/components/nav_bar.js.jsx
+++ app/assets/javascripts/components/nav_bar.js.jsx
@@ -24,25 +24,34 @@ var NavBar = React.createClass({
     var addEventButton = <li><CreateEventButton onEventSubmit = {this.handleEventSubmit}/></li>;
     var venues = this.props.loggedIn.venues;
 
-    var loggedIn =
-      <nav className="navigation pink accent-2">
-        <div className="brand-logo center">mic_drop </div>
-        <ul className="right">
-          <li><span className>{this.props.loggedIn.username}</span></li>
-          <li><LogoutButton onUserLogin = {this.handleUserLogin} /></li>
-          { loggedIn && venues.length > 0 ? addEventButton : null}
-        </ul>
-        <ul className="left">
-          <li><MyPlacesButton loggedIn = {this.props.loggedIn} /></li>
-        </ul>
-      </nav>;
-
-
+    if (this.props.loggedIn.picture_url){
+      var loggedIn =
+      <ul className="right">
+        <li>
+          <img className="responsive-img circle" src={this.props.loggedIn.picture_url} />
+        </li>
+        <li><LogoutButton onUserLogin = {this.handleUserLogin} /></li>
+        { loggedIn && venues.length > 0 ? addEventButton : null}
+      </ul>
+    } else {
+      var loggedIn =
+      <ul className="right">
+        <li><span className>{this.props.loggedIn.username}</span></li>
+        <li><LogoutButton onUserLogin = {this.handleUserLogin} /></li>
+        { loggedIn && venues.length > 0 ? addEventButton : null}
+      </ul>
+    };
 
     var buttonZone = (this.props.loggedIn ?  loggedIn : loggedOut);
 
     return(
-      buttonZone
+      <nav className="navigation pink accent-2">
+        <ul className="left">
+          <li><MyPlacesButton loggedIn = {this.props.loggedIn} /></li>
+        </ul>
+        <div className="brand-logo center">mic_drop </div>
+        {buttonZone}
+      </nav>
       );
     }
 });
