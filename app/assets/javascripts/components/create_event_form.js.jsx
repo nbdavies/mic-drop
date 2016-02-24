af53f722ed26adda01091795d2d0c50efc984d6a
Bob Dorff & Sabrina Unrein <pair+bobdorff.sunrein@devbootcamp.com>
2016-02-24 15:16:38 -0600

    change buttons to be indigo

--- app/assets/javascripts/components/create_event_form.js.jsx
+++ app/assets/javascripts/components/create_event_form.js.jsx
@@ -116,7 +116,7 @@ var EventForm = React.createClass({
           placeholder="tag1,tag2"
           value={this.state.tags}
           onChange={this.handleTagsChange} />
-        <input type="submit" value="Drop the mic!" className="btn-flat indigo accent-3 right" onClick={this.handleSubmit} />
+        <input type="submit" value="Drop the mic!" className="btn-flat indigo accent-3" onClick={this.handleSubmit} />
         <Button className="btn-flat indigo accent-3 right" onClick={this.props.handleCancelButtonClick}>Cancel</Button>
       </form></div>
     );
