46d3a64696bb4810adcbb1706382eefaffc6cac5
Phil Thomas <pthomas551@gmail.com>
2016-02-24 20:51:27 -0600

    fix close modal text

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -2,14 +2,14 @@ describe 'registration', :type => :feature, :js => true do
 
   it 'reveals the registration dialog when register is clicked' do
     visit '/'
-    click_button 'Close Window'
+    click_button "Find out what you're doing tonight."
     click_button 'register'
     expect(page).to have_field 'username'
   end
 
   it 'allows me to register an account' do
     visit '/'
-    click_button 'Close Window'
+    click_button "Find out what you're doing tonight."
     click_button 'register'
     fill_in 'username', with: 'testuser'
     fill_in 'email', with: 'testemail'
