e5a3f44810edc4499509c65fd37d1011473cda71
Bob Dorff & Sabrina Unrein <pair+bobdorff.sunrein@devbootcamp.com>
2016-02-24 19:54:07 -0600

    fix front page thing

--- app/assets/javascripts/components/site_guide.js.jsx
+++ app/assets/javascripts/components/site_guide.js.jsx
@@ -11,9 +11,10 @@ var SiteGuide = React.createClass({
 
   render: function(){
     return(
-      <div>
-        <h5 className="site-guide">Mic Drop helps you find cool activities in your area. Log in to RSVP for events and connect with Facebook to see which events your friends are attending. Find out what you're doing tonight.</h5>
-        <Button className="btn-flat indigo accent-3 right" onClick={this.handleCancelButtonClick}>Close Window</Button>
+      <div className="valign">
+        <h5 className="site-guide"><p>Mic Drop helps you find cool activities in your area.</p> <p>Log in to RSVP for events and connect your Facebook account to see which events your friends are attending. </p></h5>
+        <br/><br /><br />
+        <div className="center"><Button className="btn-flat indigo accent-3 center" onClick={this.handleCancelButtonClick}>Find out what you're doing tonight.</Button></div>
       </div>
     );
   }
